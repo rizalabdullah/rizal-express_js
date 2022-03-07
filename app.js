a52ee7ebec4a7590714a5463d8d7043e6c7bf6c4
rizalabdullah <ra160920@gmail.com>
2022-03-07 08:33:58 +0700

    express

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const router = require("./routes");
+
+app.use(router);
+
+app.listen(3000, () => console.log(`server http://localhost:3000`));
